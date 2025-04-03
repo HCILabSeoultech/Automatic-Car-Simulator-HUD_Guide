f77d582281906eb531afd86108a9e9200ee49d89
HCI231hong <128343961+HCI231hong@users.noreply.github.com>
2025-04-03 23:17:58 +0900

    hud 커브 표현
    왼쪽만 테스트로 7가지로 나눠서 표현해봄..

--- Assets/Scripts/HUD_guideLine_Controller.cs
+++ Assets/Scripts/HUD_guideLine_Controller.cs
@@ -6,7 +6,13 @@ public class HUD_guideLine_Controller : MonoBehaviour
 {
     public GameObject[] allHUDImages;
 
-    public GameObject leftHard;
+    public GameObject leftHard1;
+    public GameObject leftHard2;
+    public GameObject leftHard3;
+    public GameObject leftHard4;
+    public GameObject leftHard5;
+    public GameObject leftHard6;
+    public GameObject leftHard7;
 
     public GameObject straight;
   
@@ -15,7 +21,7 @@ public class HUD_guideLine_Controller : MonoBehaviour
     [SerializeField] private DemoCarController controller;
     private void FixedUpdate()
     {
-        float steeringAngle = controller.rawSteeringInput * 450;
+        float steeringAngle = controller.rawSteeringInput * 30;
         
         Debug.Log(steeringAngle);
 
@@ -24,8 +30,22 @@ public class HUD_guideLine_Controller : MonoBehaviour
     void UpdateHUDBySteering(float steeringAngle)
     {
         // 예시 범위: -30~30도
-        if (steeringAngle < -20)
-            ShowHUD(leftHard);
+        if (steeringAngle < -4 && steeringAngle>= -8)
+            ShowHUD(leftHard1);
+        else if (steeringAngle < -8 && steeringAngle>= -12)
+            ShowHUD(leftHard2);
+        else if(steeringAngle < -12 && steeringAngle>= -16)
+            ShowHUD(leftHard3);
+        else if(steeringAngle < -16 && steeringAngle>= -20)
+            ShowHUD(leftHard4);
+        else if(steeringAngle < -20 && steeringAngle>= -24)
+            ShowHUD(leftHard5);
+        else if(steeringAngle < -24 && steeringAngle>= -28)
+            ShowHUD(leftHard6);
+        else if(steeringAngle < -28)
+            ShowHUD(leftHard7);
+
+    
 
         else if (steeringAngle > 20)
             ShowHUD(rightHard);
